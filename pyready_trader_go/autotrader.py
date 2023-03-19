f0d8eb31d5b915955d7ba7c97a18fedb500bee51
Ethan Lee <ealee0205@berkeley.edu>
2023-03-19 02:46:54 -0700

    final version

--- pyready_trader_go/autotrader.py
+++ pyready_trader_go/autotrader.py
@@ -18,6 +18,8 @@
 import asyncio
 import itertools
 
+import math
+
 from typing import List
 
 from ready_trader_go import BaseAutoTrader, Instrument, Lifespan, MAXIMUM_ASK, MINIMUM_BID, Side
@@ -28,6 +30,10 @@ POSITION_LIMIT = 100
 TICK_SIZE_IN_CENTS = 100
 MIN_BID_NEAREST_TICK = (MINIMUM_BID + TICK_SIZE_IN_CENTS) // TICK_SIZE_IN_CENTS * TICK_SIZE_IN_CENTS
 MAX_ASK_NEAREST_TICK = MAXIMUM_ASK // TICK_SIZE_IN_CENTS * TICK_SIZE_IN_CENTS
+
+SPREAD_LIMIT = 100
+EXIT_LIMIT = -400
+
 
 
 class AutoTrader(BaseAutoTrader):
@@ -46,7 +52,53 @@ class AutoTrader(BaseAutoTrader):
         self.order_ids = itertools.count(1)
         self.bids = set()
         self.asks = set()
-        self.ask_id = self.ask_price = self.bid_id = self.bid_price = self.position = 0
+        self.ask_id = self.ask_price = self.bid_id = self.bid_price = self.position = self.hedge = 0
+        self.ticks = itertools.count(1)
+
+        self.hbids = set()
+        self.hasks = set()
+        self.hask_id = self.hbid_id = 0
+
+        self.fut_bid = 0
+        self.fut_ask = 0
+        self.etf_ask = 0
+        self.etf_bid = 0
+
+        self.etf_price = 0
+        self.fut_price = 0
+
+        self.spread = 0
+
+        self.fut_etf_spread50 = []
+        self.fut_etf_ratio = 1
+        self.fut_etf_ratio_sum = 0
+        self.fut_etf_ratio_avg = 0
+
+        self.ratio50 = []
+        self.sum_squares = 0
+        self.ratio_sd = 0
+        self.z_score = 0
+        self.prev_z = 0
+
+        self.open = 0
+        self.unexposed = 0
+
+
+
+
+        self.ready = False
+    
+    def etf_buy(self, volume):
+        if self.bid_id == 0 and self.position + volume < POSITION_LIMIT:
+                self.bid_id = next(self.order_ids)
+                self.send_insert_order(self.bid_id, Side.BUY, self.etf_ask, volume, Lifespan.FAK)
+                self.bids.add(self.bid_id)
+    
+    def etf_sell(self, volume):
+        if self.ask_id == 0 and self.position - volume > -POSITION_LIMIT:
+                self.ask_id = next(self.order_ids)
+                self.send_insert_order(self.ask_id, Side.SELL, self.etf_bid, volume, Lifespan.FAK)
+                self.asks.add(self.ask_id)
 
     def on_error_message(self, client_order_id: int, error_message: bytes) -> None:
         """Called when the exchange detects an error.
@@ -68,6 +120,26 @@ class AutoTrader(BaseAutoTrader):
         self.logger.info("received hedge filled for order %d with average price %d and volume %d", client_order_id,
                          price, volume)
 
+        
+        if volume == 0:
+            print("error hedge")
+            
+            if self.hedge > self.position and (self.hedge * -1) + (self.hedge - self.position) < POSITION_LIMIT:
+                self.hbid_id = next(self.order_ids)
+                self.hbids.add(self.hbid_id)
+                self.send_hedge_order(self.hbid_id, Side.BID, self.fut_ask, self.hedge - self.position)
+            elif self.hedge < self.position and (self.hedge * -1) - (self.position - self.hedge) > -POSITION_LIMIT:
+                self.hask_id = next(self.order_ids)
+                self.hasks.add(self.hask_id)
+                self.send_hedge_order(self.hask_id, Side.ASK, self.fut_bid, self.position - self.hedge)
+
+        if client_order_id in self.hbids:
+            self.hedge -= volume
+        elif client_order_id in self.hasks:
+            self.hedge += volume
+
+        # print(self.position, self.hedge)
+
     def on_order_book_update_message(self, instrument: int, sequence_number: int, ask_prices: List[int],
                                      ask_volumes: List[int], bid_prices: List[int], bid_volumes: List[int]) -> None:
         """Called periodically to report the status of an order book.
@@ -79,29 +151,108 @@ class AutoTrader(BaseAutoTrader):
         """
         self.logger.info("received order book for instrument %d with sequence number %d", instrument,
                          sequence_number)
+
+
+        
         if instrument == Instrument.FUTURE:
-            price_adjustment = - (self.position // LOT_SIZE) * TICK_SIZE_IN_CENTS
-            new_bid_price = bid_prices[0] + price_adjustment if bid_prices[0] != 0 else 0
-            new_ask_price = ask_prices[0] + price_adjustment if ask_prices[0] != 0 else 0
+            self.fut_bid = bid_prices[0]
+            self.fut_ask = ask_prices[0]
+            self.fut_price = (self.fut_ask + self.fut_bid) / 2
 
-            if self.bid_id != 0 and new_bid_price not in (self.bid_price, 0):
-                self.send_cancel_order(self.bid_id)
-                self.bid_id = 0
-            if self.ask_id != 0 and new_ask_price not in (self.ask_price, 0):
-                self.send_cancel_order(self.ask_id)
-                self.ask_id = 0
+        if instrument == Instrument.ETF:
+            self.etf_ask = ask_prices[0]
+            self.etf_bid = bid_prices[0]
+            self.etf_price = (self.etf_ask + self.etf_bid) / 2
 
-            if self.bid_id == 0 and new_bid_price != 0 and self.position < POSITION_LIMIT:
-                self.bid_id = next(self.order_ids)
-                self.bid_price = new_bid_price
-                self.send_insert_order(self.bid_id, Side.BUY, new_bid_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
-                self.bids.add(self.bid_id)
+        # elif self.etf_ask < self.fut_bid:
+        #     self.etf_buy(4)
+        # elif self.etf_bid > self.fut_ask:
+        #     self.etf_sell(4)
+        
 
-            if self.ask_id == 0 and new_ask_price != 0 and self.position > -POSITION_LIMIT:
-                self.ask_id = next(self.order_ids)
-                self.ask_price = new_ask_price
-                self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
-                self.asks.add(self.ask_id)
+        
+        '''calculate the price ratios'''
+        if self.etf_price != 0:
+            self.fut_etf_ratio = self.fut_price/self.etf_price
+            self.fut_etf_ratio_sum += self.fut_etf_ratio
+            self.fut_etf_spread50.append(self.fut_etf_ratio)
+            if len(self.fut_etf_spread50) > 50:
+                self.ready = True
+                self.fut_etf_ratio_sum -= self.fut_etf_spread50.pop(0)
+
+        if self.ready:
+            square = (self.fut_etf_ratio - self.fut_etf_ratio_avg) ** 2
+            self.sum_squares += square
+            self.ratio50.append(square)
+            if len(self.ratio50) > 50:
+                self.sum_squares -= self.ratio50.pop(0)
+            
+            
+            self.ratio_sd = math.sqrt(self.sum_squares / 50)
+            self.z_score = (self.fut_etf_ratio - self.fut_etf_ratio_avg) / self.ratio_sd
+
+        self.fut_etf_ratio_avg = self.fut_etf_ratio_sum / 50
+
+        # print(f' avg: {self.fut_etf_ratio_avg} ratio: {self.fut_etf_ratio} sd: {self.ratio_sd} zscore: {self.z_score}')
+        # print(f'zscore: {self.z_score} sumsquares: {self.sum_squares}')
+
+        '''Short when z-score above 1 and long when z-score below 1.'''
+        #right now, I think it's losing money on trades where the prices are near the convergence but the ask/bid spread is negative, so it trades at a loss
+
+        if self.ready and self.z_score > 1.75 and self.etf_ask < self.fut_bid:
+            self.etf_buy(30)
+        elif self.ready and self.z_score < -1.75 and self.etf_ask < self.fut_bid and self.position > 0:
+            self.etf_sell(30)
+        elif self.ready and self.z_score > 1.75 and self.etf_bid > self.fut_ask and self.position < 0:
+            self.etf_buy(30)
+        elif self.ready and self.z_score < -1.75 and self.etf_bid > self.fut_ask:
+            self.etf_sell(30)
+        
+        elif self.ready and self.z_score > 1.25 and self.etf_ask < self.fut_bid:
+            self.etf_buy(20)
+        elif self.ready and self.z_score < -1.25 and self.etf_ask < self.fut_bid and self.position > 0:
+            self.etf_sell(20)
+        elif self.ready and self.z_score > 1.25 and self.etf_bid > self.fut_ask and self.position < 0:
+            self.etf_buy(20)
+        elif self.ready and self.z_score < -1.25 and self.etf_bid > self.fut_ask:
+            self.etf_sell(20)
+        
+        elif self.ready and self.z_score > 0.75 and self.etf_ask < self.fut_bid:
+            self.etf_buy(10)
+        elif self.ready and self.z_score < -0.75 and self.etf_ask < self.fut_bid and self.position > 0:
+            self.etf_sell(10)
+        elif self.ready and self.z_score > 0.75 and self.etf_bid > self.fut_ask and self.position < 0:
+            self.etf_buy(10)
+        elif self.ready and self.z_score < -0.75 and self.etf_bid > self.fut_ask:
+            self.etf_sell(10)
+        
+        # # elif self.ready and self.z_score > 0.5 and self.etf_ask < self.fut_bid:
+        # #     self.etf_buy(5)
+        # # elif self.ready and self.z_score < -0.5 and self.etf_ask < self.fut_bid and self.position > 0:
+        # #     self.etf_sell(5)
+        # # elif self.ready and self.z_score > 0.5 and self.etf_bid > self.fut_ask and self.position < 0:
+        # #     self.etf_buy(5)
+        # # elif self.ready and self.z_score < 0.5 and self.etf_bid > self.fut_ask:
+        # #     self.etf_sell(5)
+        
+
+
+
+
+        # # elif self.ready and self.z_score > self.prev_z and self.z_score > 0.5:
+        # #     self.etf_buy(1)
+
+        # # elif self.ready and self.z_score < self.prev_z and self.z_score < -0.5:
+        # #     self.etf_sell(1)
+
+
+
+        
+        
+
+        # self.prev_z = self.z_score
+
+
 
     def on_order_filled_message(self, client_order_id: int, price: int, volume: int) -> None:
         """Called when one of your orders is filled, partially or fully.
@@ -114,10 +265,20 @@ class AutoTrader(BaseAutoTrader):
                          price, volume)
         if client_order_id in self.bids:
             self.position += volume
-            self.send_hedge_order(next(self.order_ids), Side.ASK, MIN_BID_NEAREST_TICK, volume)
+            if (self.hedge * -1) - volume > -POSITION_LIMIT:
+                self.hask_id = next(self.order_ids)
+                self.hasks.add(self.hask_id)
+                self.send_hedge_order(self.hask_id, Side.ASK, self.fut_bid, volume)
+            # print(f'spread: {self.fut_bid - self.etf_ask}')
+
         elif client_order_id in self.asks:
             self.position -= volume
-            self.send_hedge_order(next(self.order_ids), Side.BID, MAX_ASK_NEAREST_TICK, volume)
+            if (self.hedge * -1) + volume < POSITION_LIMIT:
+                self.hbid_id = next(self.order_ids)
+                self.hbids.add(self.hbid_id)
+                self.send_hedge_order(self.hbid_id, Side.BID, self.fut_ask, volume)
+            # print(f'spread: {self.etf_bid - self.fut_ask}')
+            
 
     def on_order_status_message(self, client_order_id: int, fill_volume: int, remaining_volume: int,
                                 fees: int) -> None:
@@ -142,6 +303,7 @@ class AutoTrader(BaseAutoTrader):
             self.bids.discard(client_order_id)
             self.asks.discard(client_order_id)
 
+
     def on_trade_ticks_message(self, instrument: int, sequence_number: int, ask_prices: List[int],
                                ask_volumes: List[int], bid_prices: List[int], bid_volumes: List[int]) -> None:
         """Called periodically when there is trading activity on the market.
@@ -155,3 +317,13 @@ class AutoTrader(BaseAutoTrader):
         """
         self.logger.info("received trade ticks for instrument %d with sequence number %d", instrument,
                          sequence_number)
+                
+        
+
+
+    
+
+        
+
+        
+        
